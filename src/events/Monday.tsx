b506d2090f3b054c9a93c32bf5fd46ad0176cdb0
ai-cybersecurity <kerimows99@gmail.com>
2023-12-17 00:42:43 +0500

    finished icons of monday events

--- src/events/Monday.tsx
+++ src/events/Monday.tsx
@@ -4,7 +4,9 @@ import '../css/weekDays.css'
 import { useTranslation } from 'react-i18next';
 
 // react-icons
-import { MdOutlineFestival } from "react-icons/md";
+import { LiaBirthdayCakeSolid } from "react-icons/lia";
+import { LuMusic4 } from "react-icons/lu";
+import { CgGames } from "react-icons/cg";
 
 const Monday = () => {
 
@@ -19,7 +21,7 @@ const Monday = () => {
             contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
             date="14:30"
-            icon={<MdOutlineFestival />}
+            icon={<LiaBirthdayCakeSolid />}
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
             <h3 className="vertical-timeline-element-title">Opening Ceremony</h3>
             <p>{t('monday.description1')}</p>
@@ -29,6 +31,7 @@ const Monday = () => {
             contentStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
             contentArrowStyle={{ borderRight: '7px solid  rgb(233, 30, 99)' }}
             date="14:30"
+            icon={<LuMusic4 />}
             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
             <h3 className="vertical-timeline-element-title">Music Festival</h3>
             <p>{t('monday.description2')}</p>
@@ -39,6 +42,7 @@ const Monday = () => {
             contentStyle={{ background: 'rgb(76, 175, 80)', color: '#fff' }}
             contentArrowStyle={{ borderRight: '7px solid  rgb(76, 175, 80)' }}
             date="14:30"
+            icon={<CgGames />}
             iconStyle={{ background: 'rgb(76, 175, 80)', color: '#fff' }}>
             <h3 className="vertical-timeline-element-title">Games</h3>
             <p>{t('monday.description3')}</p>
